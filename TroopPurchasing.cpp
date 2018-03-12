1d967d53215284ff1c8d1c854c9a810c7ea6161a
Adam Sellers (sellers3) <sellers3@coventry.ac.uk>
2018-03-12 01:49:23 +0000

    Update TroopPurchasing.cpp

--- TroopPurchasing.cpp
+++ TroopPurchasing.cpp
@@ -21,12 +21,38 @@ void purchaseUnitType(int player_ID, int troop_ID, int map_ID)
         sqlite::sqlite db(file);
         auto cur1=db.get_statement();
         auto cur2=db.get_statement();
+        auto cur3=db.get_statement();
+        auto cur4=db.get_statement();
         
         cur1->set_sql("INSERT INTO player_army (player_ID, military_ID, map_ID)"
-                     "VALUES (?,?,?)");
+                      "VALUES (?,?,?)");
+        cur1->prepare();
         cur1->bind(1, player_ID);
         cur1->bind(2, troop_ID);
         cur1->bind(3, map_ID);
+        
+        cur2->set_sql("SELECT cost 
+                      "FROM military_Units"
+                      "WHERE ID = ?");
+        cur2->prepare();
+        cur2->bind(1, troop_ID);
+        cur2->step();
+        
+        int cost = cur2->get_int(0);
+        
+        cur3->set_sql("SELECT total_Money"
+                      "FROM player")
+        cur3->prepare();
+        cur3->step();
+        
+        int total_Money = cur3->get_int(0);
+        total_Money = total_Money - cost
+        
+        cur4->set_sql("UPDATE player"
+                      "SET total_Money = ?")
+        cur4->prepare();
+        cur4->bind(1, total_Money)
+            
         
     }
     catch(sqlite::exception e)
