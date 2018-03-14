e4a3231330aa20ed1f32ccbd2b1ee9a240a3ea3d
Adam Sellers (sellers3) <sellers3@coventry.ac.uk>
2018-03-14 21:34:19 +0000

    Update BattleFunctionality.cpp

--- BattleFunctionality.cpp
+++ BattleFunctionality.cpp
@@ -13,7 +13,6 @@ vector<int> troopCounter(int map_ID, int player_ID, string AIorPlayer) ///Counts
         sqlite::sqlite db(file);
         auto cur1=db.get_statement();
 
-        cout << "TroopTest1" << endl;
         int totalAtk = 0;
         int totalDef = 0;
         
@@ -25,7 +24,6 @@ vector<int> troopCounter(int map_ID, int player_ID, string AIorPlayer) ///Counts
                           "WHERE player_Army.player_ID = ? ");        
             cur1->prepare();
             cur1->bind(1, player_ID);
-            cout << "TroopTest2" << endl;
             while( cur1->step() )
             {
                 int attackValue = cur1->get_int(0);
@@ -34,7 +32,6 @@ vector<int> troopCounter(int map_ID, int player_ID, string AIorPlayer) ///Counts
                 totalDef = defenceValue + totalDef;
             }
             vector<int> Atk_Def = {totalAtk,totalDef};
-            cout << Atk_Def[0] << " || " << Atk_Def[1] << endl;
             return Atk_Def;
             }
         
@@ -46,7 +43,6 @@ vector<int> troopCounter(int map_ID, int player_ID, string AIorPlayer) ///Counts
                           "WHERE ai_Army.player_ID = ? ");        
             cur1->prepare();
             cur1->bind(1, player_ID);
-            cout << "TroopTest2" << endl;
             while( cur1->step() )
             {
                 int attackValue = cur1->get_int(0);
@@ -55,7 +51,6 @@ vector<int> troopCounter(int map_ID, int player_ID, string AIorPlayer) ///Counts
                 totalDef = defenceValue + totalDef;
             }
             vector<int> Atk_Def = {totalAtk,totalDef};
-            cout << Atk_Def[0] << " || " << Atk_Def[1] << endl;
             return Atk_Def;
             }
 
@@ -76,13 +71,10 @@ int delTroops(int threshold, int map_ID, int player_ID, int AI_map_ID, string AI
         sqlite::sqlite db(file);
         auto cur1=db.get_statement();
         
-        cout << "TestDelete1" << endl;
-        
         if( AIorPlayer == "AI")
         {
             vector<int> armyValues = troopCounter(map_ID, player_ID,  "Player");
             int attackValue = armyValues[0];
-            cout << attackValue << " !!!!!!! " << threshold << endl;
             while(attackValue>threshold)
             {
                 cur1->set_sql("DELETE FROM player_Army "
@@ -94,12 +86,11 @@ int delTroops(int threshold, int map_ID, int player_ID, int AI_map_ID, string AI
                  cur1->bind(2, player_ID);
                  cur1->step();
                  cur1->reset();
-                 cout << "DoneBattleAI " << "Threshold: " << threshold << endl;
                  vector<int> armyValues = troopCounter(map_ID, player_ID, "Player");
                  attackValue = armyValues[0];
             }
         }
-
+        
         else if ( AIorPlayer == "Player")
         {
             vector<int> armyValues = troopCounter(AI_map_ID, player_ID, "AI");
@@ -116,14 +107,10 @@ int delTroops(int threshold, int map_ID, int player_ID, int AI_map_ID, string AI
                  cur1->bind(2, player_ID);
                  cur1->step();
                  cur1->reset();
-                 cout << "DoneBattlePlayer " << "Threshold: " << threshold << endl;
                  vector<int> armyValues = troopCounter(AI_map_ID, player_ID, "AI");
                  attackValue = armyValues[0];
             }
         }
-        
-        cout << "done" << endl << flush;
-        
     }
     catch(sqlite::exception e)
     {
@@ -134,57 +121,71 @@ int delTroops(int threshold, int map_ID, int player_ID, int AI_map_ID, string AI
 
 int battle(int map_ID, int player_ID, int AI_map_ID) ///Calculates the threshold for the loss of troops to be used to delete troops from database
 {
-    vector<int> AI_Army = troopCounter(AI_map_ID, player_ID, "AI");
-    int AIAttackTotal = AI_Army[0];
-    int AIDefenceTotal = AI_Army[1];
-    vector<int> player_Army = troopCounter(map_ID, player_ID, "Player");
-    int playerAttackTotal = player_Army[0];
-    int playerDefenceTotal = player_Army[1];
-    cout << "testBattle1" << endl;
-    int AIAttack = rand() % AIAttackTotal/10 + AIAttackTotal;
-    int AIDefence = rand() % AIDefenceTotal/10 + AIDefenceTotal;
-    int playerAttack = rand() % playerAttackTotal/10 + playerAttackTotal;
-    int playerDefence = rand() % playerDefenceTotal/10 + playerDefenceTotal;
-    
-    cout << "Player Attack roll is: " << playerAttack << endl;
-    cout << "AI Attack roll is: " << AIAttack << endl;
-    cout << "Player Def roll is: " << playerDefence << endl;
-    cout << "AI Def roll is: " << AIDefence << endl;    
-    cout << "testBattle2" << endl;
-    if (AIAttack>playerAttack)
+    string file = "civilwarofMalibu.db";
+    try
     {
-        cout << "TestBattleAIWin" << endl;
-        if (AIAttack>2*playerAttack)
+        sqlite::sqlite db(file);
+        auto cur1=db.get_statement();
+
+        vector<int> AI_Army = troopCounter(AI_map_ID, player_ID, "AI");
+        int AIAttackTotal = AI_Army[0];
+        int AIDefenceTotal = AI_Army[1];
+        vector<int> player_Army = troopCounter(map_ID, player_ID, "Player");
+        int playerAttackTotal = player_Army[0];
+        int playerDefenceTotal = player_Army[1];
+        int AIAttack = rand() % AIAttackTotal/10 + AIAttackTotal;
+        int AIDefence = rand() % AIDefenceTotal/10 + AIDefenceTotal;
+        int playerAttack = rand() % playerAttackTotal/10 + playerAttackTotal;
+        int playerDefence = rand() % playerDefenceTotal/10 + playerDefenceTotal;
+
+        cout << "Player Attack roll is: " << playerAttack << endl;
+        cout << "AI Attack roll is: " << AIAttack << endl;
+        cout << "Player Def roll is: " << playerDefence << endl;
+        cout << "AI Def roll is: " << AIDefence << endl;    
+        if (AIAttack>playerAttack)
         {
-            cout << "AI wiped out your army!" << endl;
+            if (AIAttack>2*playerAttack)
+            {
+                cout << "AI wiped out your army!" << endl;
+                cur1->set_sql("DELETE FROM player_Army "
+                              "WHERE map_ID = ? AND player_ID = ? ");
+                cur1->bind(1, map_ID);
+                cur1->bind(2, player_ID);
+                cur1->step();
+            }
+            else
+            {
+                cout << "AI won the battle!" << endl;
+                int threshold = static_cast<double>(AIAttack) - (static_cast<double>(AIAttack)*(static_cast<double>(AIDefence)/static_cast<double>(playerAttack)));
+                delTroops(threshold, map_ID, player_ID, AI_map_ID, "AI");
+            }
         }
-        else
+        else if (playerAttack>AIAttack)
         {
-            cout << "AI won the battle!" << endl;
-            int threshold = static_cast<double>(AIAttack) - (static_cast<double>(AIAttack)*(static_cast<double>(AIDefence)/static_cast<double>(playerAttack)));
-            cout << "Threshold : " << threshold << endl << flush;
-            delTroops(threshold, map_ID, player_ID, AI_map_ID, "AI");
+            if (playerAttack>2*AIAttack)
+            {
+                cur1->set_sql("DELETE FROM ai_Army "
+                              "WHERE map_ID = ? AND player_ID = ? ");
+                cur1->prepare();
+                cur1->bind(1, AI_map_ID);
+                cur1->bind(2, player_ID);
+                cur1->step();
+            }
+            else
+            {
+                cout << "You won the battle!" << endl;
+                int threshold = static_cast<double>(playerAttack) - (static_cast<double>(playerAttack)*(static_cast<double>(playerDefence)/static_cast<double>(AIAttack)));
+                delTroops(threshold, map_ID, player_ID, AI_map_ID, "Player");
+            }
+        }
+        else if (playerAttack==AIAttack)
+        {
+            cout << "Battle was a draw!" << endl;
         }
     }
-    else if (playerAttack>AIAttack)
+    catch(sqlite::exception e)
     {
-        cout << "TestBattlePlayerWin" << endl;
-        if (playerAttack>2*AIAttack)
-        {
-            cout<< "You wiped out the AI army!" << endl;
-            
-        }
-        else
-        {
-            cout << "You won the battle!" << endl;
-            int threshold = static_cast<double>(playerAttack) - (static_cast<double>(playerAttack)*(static_cast<double>(playerDefence)/static_cast<double>(AIAttack)));
-            cout << "Threshold : " << threshold << endl << flush;
-            delTroops(threshold, map_ID, player_ID, AI_map_ID, "Player");
-        }
-    }
-    else if (playerAttack==AIAttack)
-    {
-        cout << "Whatever" << endl;
+        std::cerr << e.what() << endl;
     }
 }
 
