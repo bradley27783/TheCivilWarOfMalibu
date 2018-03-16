bdee2cc9d369fb83cd4e8e4cfc4ee694e6aea5db
Mihail Bulanov (bulanovm) <bulanovm@coventry.ac.uk>
2018-03-16 06:28:10 +0000

    Update and rename shop.cpp to ProductionUnitShop.cpp
    Guys, sorry that I haven't been active these days. I had a lot of stuff on my head and wasn't been able to be of any use to the group. I am uploading this as my code for the production unit shop, but I cant integrate it. Sorry.

---
+++ ProductionUnitShop.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include "libsqlite.hpp"
+using namespace std;
+
+int main()
+{
+    int a;
+    string sqliteFile = "civilwarofMalibu.db";
+    
+    try
+    {
+        sqlite::sqlite db(sqliteFile);
+        auto cur=db.get_statement();
+        
+        cur->set_sql("SELECT * "
+                     "FROM production_Units ");
+        cur->prepare();
+        cout << "              " << "Income  /  Cost " << endl;
+        while ( cur->step() )
+            cout << cur->get_int(0) << " " << cur->get_text(1) << "   " <<cur->get_int(2)<< "   " << cur->get_int(3) << endl;
+    }
+    catch( sqlite::exception e)      // catch all sql issues
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    
+    if(a==1){
+        cout << "How many would you like to buy? " << endl;
+        int b;
+        cin >> b;
+        string sqliteFile = "civilwarofMalibu.db";
+    
+        try                                                 
+        {
+            sqlite::sqlite db(sqliteFile);
+            auto cur=db.get_statement();
+        
+            cur->set_sql("INSERT INTO player_income "
+                         "VALUES (?)");
+            cur->bind(1,b);
+        }
+        
+    else if(a==2){
+        cout << "How many would you like to buy? " << endl;
+        int b;
+        cin >> b;
+        string sqliteFile = "civilwarofMalibu.db";
+    
+        try                                                 
+        {
+            sqlite::sqlite db(sqliteFile);
+            auto cur=db.get_statement();
+        
+            cur->set_sql("INSERT INTO player_income "
+                         "VALUES (?)");
+            cur->bind(1,b);
+        }
+        
+    else if(a==3){
+        cout << "How many would you like to buy? " << endl;
+        int b;
+        cin >> b;
+        string sqliteFile = "civilwarofMalibu.db";
+    
+        try                                                 
+        {
+            sqlite::sqlite db(sqliteFile);
+            auto cur=db.get_statement();
+        
+            cur->set_sql("INSERT INTO player_income "
+                         "VALUES (?)");
+            cur->bind(1,b);
+        }
+
+    return 0;
+}
